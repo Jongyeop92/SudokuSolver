8d59f243423b20c917852d3c87e3712deb1f14d5
AIRSF(한종엽) <airsf-m13@adi.jejuair.net>
2017-04-24 17:32:51 +0900

    almost complete

--- main.py
+++ main.py
@@ -1,45 +1,148 @@
 # -*- coding: utf8 -*-
 
 
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
-digits   = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+import copy
+
+
+digits = "123456789"
+rows   = "ABCDEFGHI"
+cols   = digits
+
+def cross(A, B):
+    return [a + b for a in A for b in B]
+
+squares = cross(rows, cols)
+
+unitlist = [[r + c for c in cols] for r in rows] + \
+           [[r + c for r in rows] for c in cols] + \
+           [cross(rs, cs) for rs in ["ABC", "DEF", "GHI"] for cs in ["123", "456", "789"]]
+
+units = dict((s, [u for u in unitlist if s in u]) for s in squares)
+peers = dict((s, set(sum(units[s], [])) - set([s])) for s in squares)
+
 
 class Sudoku:
 
     def __init__(self):
-        self.boardKeyList = [a + d for a in alphabet
-                                   for d in digits]
-
-        self.board = dict
-
+        self.board = dict((s, digits) for s in squares)
 
     def setData(self, data):
-        pass
+        validData = ""
+
+        for d in data:
+            if d in digits or d in ". ":
+                validData += d
+
+        assert len(validData) == 81
+
+        for s, d in zip(squares, validData):
+            if d in digits:
+                self.assign(s, d)
+
+    def assign(self, s, d):
+        otherValue = self.board[s].replace(d, "")
+
+        if all(self.eliminate(s, d2) for d2 in otherValue) and len(self.board[s]) == 1:
+            if all(self.eliminate(s, d) for s in peers[s]):
+                return True
+
+        return False
+
+    def eliminate(self, s, d):
+        if d not in self.board[s]:
+            return True
+        
+        self.board[s] = self.board[s].replace(d, "")
+
+        if len(self.board[s]) == 0:
+            return False
+        elif len(self.board[s]) == 1:
+            self.assign(s, self.board[s][0])
+
+        for u in units[s]:
+            dplaces = [s for s in u if d in self.board[s]]
+
+            if len(dplaces) == 0:
+                return False
+            elif len(dplaces) == 1:
+                if not self.assign(dplaces[0], d):
+                    return False
+
+        return True
 
     def isEnd(self):
-        return None
+        return all(len(self.board[s]) == 1 for s in squares)
+
+    def show(self):
+        width = 1 + max(len(self.board[s]) for s in squares)
+        line = "+".join(["-" * (width * 3)] * 3)
+        
+        for r in rows:
+            print "".join(self.board[r + c].center(width, ' ') + ("|" if c in "36"  else "") for c in cols)
+            if r in "CF":
+                print line
+        print
+
+
+def solve(sudoku):
+
+    if sudoku.isEnd():
+        return sudoku
+
+    _, s = min((len(sudoku.board[s]), s) for s in squares if len(sudoku.board[s]) > 1)
+
+    for d in sudoku.board[s]:
+        copySudoku = copy.deepcopy(sudoku)
+        copySudoku.assign(s, d)
+
+        result = solve(copySudoku)
+
+        if result: return result
+
+    return False
 
 
 def test():
 
-    sudoku = Sudoku()
-
-    assert len(sudoku.boardKeyList) == 81
-
-    print sudoku.boardKeyList
+    assert len(squares)      == 81
+    assert len(unitlist)     == 27
+    assert all(len(peers[s]) == 20 for s in squares)
     
-    sudoku.setData('')
 
-    assert sudoku.isEnd() == True
+    sudoku = Sudoku()
+    assert sudoku.isEnd() == False
+    
+    sudoku.setData(".578..9.....3..2.83.8.....1289.6.51.5.3...4.6.64.5.8278.....3.57.2..5.....5..768.")
+    assert sudoku.isEnd() == False
+
+    sudoku.show()
+
+    result = solve(sudoku)
+    assert result.isEnd() == True
+    result.show()
+    
 
     print "Success"
 
 
 def main():
 
-    pass
+    data = "..7......5..32......4..9752....9......37.25......4....7429..8......74..9......4.."
+
+    sudoku = Sudoku()
+    sudoku.setData(data)
+
+    sudoku.show()
+
+    result = solve(sudoku)
+
+    if result:
+        result.show()
+        print "Success"
+    else:
+        print "Fail"
 
 
 if __name__ == "__main__":
-    test()
-    #main()
+    #test()
+    main()
